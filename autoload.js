f99c3608ef1cb9bf8535702aa89f1dd9c352ef51
ltggggg <35888133+ltggggg@users.noreply.github.com>
2018-10-28 00:45:46 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,8 +1,8 @@
-$("<link>").attr({href: "https://www.example.com/path/to/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+$("<link>").attr({href: "https://github.com/ltggggg/live2d-widget.git/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
 //waifu.css的绝对路径
 
 $.ajax({
-	url: 'https://www.example.com/path/to/live2d.min.js',
+	url: 'https://github.com/ltggggg/live2d-widget.git/live2d.min.js',
 	dataType:"script",
 	cache: true,
 	async: false
@@ -10,7 +10,7 @@ $.ajax({
 //live2d.min.js的绝对路径
 
 $.ajax({
-	url: 'https://www.example.com/path/to/waifu-tips.js',
+	url: 'https://github.com/ltggggg/live2d-widget.git/waifu-tips.js',
 	dataType:"script",
 	cache: true,
 	async: false
@@ -19,7 +19,7 @@ $.ajax({
 
 // 初始化看板娘，会自动加载指定目录下的 waifu-tips.json
 $(window).on("load", function() {
-	initWidget("/lib/waifu/", "https://api.fghrsh.net/live2d");
+	initWidget("/waifu/", "https://api.fghrsh.net/live2d");
 });
 //initWidget第一个参数为waifu-tips.json所在的文件夹，第二个参数为api地址
 //api后端可自行搭建，参考https://github.com/fghrsh/live2d_api
